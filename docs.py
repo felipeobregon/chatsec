c02568e961a50d5e6cba3e70247eb86236ae1f0a
felipe <felipe.a.obregon@gmail.com>
2023-05-02 16:04:22 -0400

    add plan

---
+++ docs.py
@@ -0,0 +1,12 @@
+from langchain.document_loaders import UnstructuredEPubLoader
+
+# goal: ask questions to a book
+
+'''
+
+you need all your data as chunks of text.   
+
+receive an EPUB and output a json file that contains
+an array of chunks, include metadata
+
+'''
