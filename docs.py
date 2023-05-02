e0447bcf86360a897e70a6186f23b0963b126464
felipe <felipe.a.obregon@gmail.com>
2023-05-02 16:36:10 -0400

    add chunking

--- docs.py
+++ docs.py
@@ -10,3 +10,18 @@ receive an EPUB and output a json file that contains
 an array of chunks, include metadata
 
 '''
+# load
+loader = UnstructuredEPubLoader()
+
+book = loader.load()
+
+# chunk
+from langchain.text_splitter import CharacterTextSplitter
+
+ts = CharacterTextSplitter(
+    chunk_size=1000
+)
+
+docs = ts.split_documents(book)
+
+# embed
