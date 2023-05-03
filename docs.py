55491fb2cba2a6f85dbf44d16ad3103535ec8c4f
felipe <felipe.a.obregon@gmail.com>
2023-05-03 08:35:01 -0400

    set api keys from envinroment var

--- docs.py
+++ docs.py
@@ -6,6 +6,19 @@ receive an EPUB and output a json file that contains
 an array of chunks, include metadata
 
 '''
+# set environment variables and api keys
+import os
+import getpass
+
+if 'OPENAI_API_KEY' not in os.environ:
+    os.environ['OPENAI_API_KEY'] = getpass.getpass('OpenAI API Key: ')
+
+PINECONE_API_KEY = os.environ['PINECONE_API_KEY']
+PINECONE_ENV = os.environ['PINECONE_ENV']
+
+#PINECONE_API_KEY = getpass.getpass('Pinecone API Key:')
+#PINECONE_ENV = getpass.getpass('Pinecone Environment:')
+
 # load
 from langchain.document_loaders import UnstructuredEPubLoader
 loader = UnstructuredEPubLoader()
