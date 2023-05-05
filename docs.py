2c4f378ee91549d8993304c60bea3426924859d4
felipe <felipe.a.obregon@gmail.com>
2023-05-05 12:23:22 -0400

    clean up

--- docs.py
+++ docs.py
@@ -1,11 +1,4 @@
-'''
 
-you need all your data as chunks of text.   
-
-receive an EPUB and output a json file that contains
-an array of chunks, include metadata
-
-'''
 # set environment variables and api keys
 import os
 import getpass
@@ -16,8 +9,6 @@ if 'OPENAI_API_KEY' not in os.environ:
 PINECONE_API_KEY = os.environ['PINECONE_API_KEY']
 PINECONE_ENV = os.environ['PINECONE_ENV']
 
-#PINECONE_API_KEY = getpass.getpass('Pinecone API Key:')
-#PINECONE_ENV = getpass.getpass('Pinecone Environment:')
 
 # load
 from langchain.document_loaders import UnstructuredEPubLoader
@@ -45,9 +36,3 @@ pinecone.init(
 index_name = "chat"
 
 docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
-
-
-query = "What did the president say about Ketanji Brown Jackson"
-docs = docsearch.similarity_search(query)
-
-print(docs[0])
