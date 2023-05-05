ea7b6e14d5b4c969ae35d519d431d77936ec11f5
felipe <felipe.a.obregon@gmail.com>
2023-05-05 11:36:46 -0400

    add query cli

---
+++ upload.py
@@ -0,0 +1,29 @@
+from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+import pinecone
+import os
+
+
+# This is a CLI for sending queries to an index
+# Documents that are similar to the query are retrieved
+
+pinecone.init(api_key=os.environ['PINECONE_API_KEY'], environment=os.environ['PINECONE_ENV'])
+
+
+index_name = input('Index name: ')
+query = input('Query')
+
+index = pinecone.Index(index_name)
+embeddings = OpenAIEmbeddings()
+
+# Creates an object representing the vector store
+vs = Pinecone(index, embeddings.embed_query, "text")
+
+res = vs.similarity_search(query)
+
+
+with open('out', 'w') as f:
+    outs = ''
+    for x in res:
+        outs += (x.page_content + '\n-----\n')
+    f.write(outs)
